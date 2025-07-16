1475654ade0ceac88e1f0646415e4fdee21c6b3a
Petar Petrov <ppp1808@gmail.com>
2025-07-16 22:03:03 +0300

    Added 6 CBV, 6 FBV Renamed the CRUD TEMPLATES starting with shinobi Created home template with home_view Created comment template In forms I hash the password when account is created succesfully and made password statement where i check for match

--- shinobi_verse/forms.py
+++ shinobi_verse/forms.py
@@ -1,21 +1,63 @@
 from django import forms
+from django.contrib.auth import get_user_model
+
+from Shinobi_Network.shinobi_verse.models import Shinobi, Clan, Jutsu, Comment
+
+UserModel = get_user_model()
 
 
 class ShinobiForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = Shinobi
+        fields = ['name', 'age', 'village', 'rank', 'chakra_nature', 'clan', 'bio', 'shinobi_picture']
 
 
 class ClanForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = Clan
+        fields = ['name', 'village', 'symbol', 'description', 'founder']
 
 
 class JutsuForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = Jutsu
+        fields = ['name', 'chakra_type', 'jutsu_type', 'description']
 
 
 class CommentForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = Comment
+        fields = ['text']
 
 
 class RegisterForm(forms.ModelForm):
-    pass
+    password1 = forms.CharField(placeholder="Password", widget=forms.PasswordInput)
+    password2 = forms.CharField(placeholder='Password confirm', widget=forms.PasswordInput)
+    email = forms.EmailField(max_length=50, required=True)
+
+    class Meta:
+        model = UserModel
+        fields = ['email']
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get('password1')
+        password2 = self.cleaned_data.get('password2')
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
+
+
+class LoginForm(forms.Form):
+    email = forms.EmailField(max_length=50)
+    password = forms.CharField(widget=forms.PasswordInput)
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField(placeholder="Search", max_length=100, required=False)
--- shinobi_verse/views.py
+++ shinobi_verse/views.py
@@ -1,3 +1,139 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.contenttypes.models import ContentType
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from Shinobi_Network.shinobi_verse.forms import RegisterForm, LoginForm, ShinobiForm, CommentForm
+from Shinobi_Network.shinobi_verse.models import Shinobi, Like, Clan, Jutsu
+
 
 # Create your views here.
+
+class ShinobiListView(ListView):
+    model = Shinobi
+    template_name = 'shinobi_list.html'
+    context_object_name = 'shinobi_list'
+
+
+class ShinobiDetailView(DetailView):
+    model = Shinobi
+    template_name = 'CRUD/shinobi_detail.html'
+
+
+class ShinobiCreateView(LoginRequiredMixin, CreateView):
+    model = Shinobi
+    form_class = ShinobiForm
+    template_name = 'CRUD/shinobi_create.html'
+    success_url = reverse_lazy('shinobi_list')
+
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class ShinobiUpdateView(LoginRequiredMixin, UpdateView):
+    model = Shinobi
+    form_class = ShinobiForm
+    template_name = 'CRUD/shinobi_update.html'
+    success_url = reverse_lazy('shinobi_list')
+
+
+class ShinobiDeleteView(LoginRequiredMixin, DeleteView):
+    model = Shinobi
+    template_name = 'CRUD/shinobi_delete.html'
+    success_url = reverse_lazy('shinobi_list')
+
+
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been created!')
+            return redirect('login')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'registration/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+            user = authenticate(email=email, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('base')
+            else:
+                messages.error(request, 'Invalid Credentials')
+    else:
+        form = LoginForm()
+    return render(request, 'registration/login.html', {'form': form})
+
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('base')
+
+
+
+@login_required
+def like_view(request, model_name, object_id):
+    content_type = ContentType.objects.get(model=model_name)
+    obj = content_type.model_class().objects.filter(id=object_id)
+
+    if not obj:
+        messages.error(request, "Item not found.")
+        return redirect('home')
+
+    like, created = Like.objects.get_or_create(
+        user=request.user,
+        content_type=content_type,
+        object_id=object_id)
+
+    if not created:
+        like.delete()
+
+    return redirect(request.META['HTTP_REFERER'])
+
+@login_required
+def comment_view(request, model_name, object_id):
+    content_type = ContentType.objects.get(model=model_name)
+    obj = content_type.get_object_for_this_type(id=object_id)
+
+    if not obj:
+        messages.error(request, "Item not found.")
+        return redirect('home')
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.user = request.user
+            comment.content_type = content_type
+            comment.object_id = object_id
+            comment.save()
+            return redirect(request.META.get('HTTP_REFERER'))
+    else:
+        form = CommentForm()
+    return render(request, 'add_comment.html', {'form': form})
+
+
+def home_view(request):
+    shinobies = Shinobi.objects.order_by('-created_at')
+    clans = Clan.objects.filter(approved=True)
+    jutsus = Jutsu.objects.filter(approved=True)
+
+    context = {
+        'shinobies': shinobies,
+        'clans': clans,
+        'jutsus': jutsus,
+    }
+    return render(request, 'home.html', context)
